d30c55c577cf36fb66171d90df0d399ed07a0d48
JonasMoura-J <jonasmourat9090@gmail.com>
2020-11-30 11:31:25 -0300

    Criando operação de instanciação

--- src/main/java/com/web/SpringService/SpringServiceApplication.java
+++ src/main/java/com/web/SpringService/SpringServiceApplication.java
@@ -1,13 +1,32 @@
 package com.web.SpringService;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.web.SpringService.domain.Categoria;
+import com.web.SpringService.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringServiceApplication {
+public class SpringServiceApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository catRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
