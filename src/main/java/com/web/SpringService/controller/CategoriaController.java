0d9a7ee582dca1ecf264955b69b358c20dadea9a
JonasMoura-J <jonasmourat9090@gmail.com>
2020-11-30 09:56:49 -0300

    adicionando classe de teste

---
+++ src/main/java/com/web/SpringService/controller/CategoriaController.java
@@ -0,0 +1,11 @@
+package com.web.SpringService.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaController {
+	
+	
+}
