80e76230a53395cde641a1e81149678443326e1e
jonas.dsn.pack <jonas.dsn.pack@alterdata.com.br>
2021-04-25 17:23:35 -0300

    Adicionando classe de produto e criando associação com categoria

--- src/main/java/com/web/SpringService/SpringServiceApplication.java
+++ src/main/java/com/web/SpringService/SpringServiceApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.web.SpringService.domain.Categoria;
+import com.web.SpringService.domain.Produto;
 import com.web.SpringService.repositories.CategoriaRepository;
+import com.web.SpringService.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringServiceApplication implements CommandLineRunner{
 	
 	@Autowired
-	private CategoriaRepository catRepository;
+	CategoriaRepository catRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringServiceApplication.class, args);
@@ -25,8 +30,19 @@ public class SpringServiceApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
 		
-		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p1.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
---
+++ src/main/java/com/web/SpringService/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.web.SpringService.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.web.SpringService.domain.Categoria;
+import com.web.SpringService.domain.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Object>{
+	Categoria findById(Integer Id);
+}
