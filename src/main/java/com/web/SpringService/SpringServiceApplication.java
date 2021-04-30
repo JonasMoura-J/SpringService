741b911f37be158236d628d2a42c3e89c98d4ad7
JonasMoura-J <jonasmourat9090@gmail.com>
2021-04-30 09:33:03 -0300

    Adicionando classes de cidade e estado

--- src/main/java/com/web/SpringService/SpringServiceApplication.java
+++ src/main/java/com/web/SpringService/SpringServiceApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.web.SpringService.domain.Categoria;
+import com.web.SpringService.domain.Cidade;
+import com.web.SpringService.domain.Estado;
 import com.web.SpringService.domain.Produto;
 import com.web.SpringService.repositories.CategoriaRepository;
+import com.web.SpringService.repositories.CidadeRepository;
+import com.web.SpringService.repositories.EstadoRepository;
 import com.web.SpringService.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,7 +24,13 @@ public class SpringServiceApplication implements CommandLineRunner{
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
-
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringServiceApplication.class, args);
 	}
@@ -41,8 +51,21 @@ public class SpringServiceApplication implements CommandLineRunner{
 		p1.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
 		catRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
---
+++ src/main/java/com/web/SpringService/repositories/EstadoRepository.java
@@ -0,0 +1,13 @@
+package com.web.SpringService.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.web.SpringService.domain.Categoria;
+import com.web.SpringService.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Object>{
+	
+	@Query("SELECT a FROM Estado a WHERE a.id = ?1")
+	Estado buscarPorId(Integer id);
+}
